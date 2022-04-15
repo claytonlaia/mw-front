d9eb145c259380632fc07ec09ff6c4904ac797dc
Clayton <claytonmlaia@hotmail.com>
2022-04-15 10:58:16 -0300

    Terminada a parte que consome api

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import axios from 'axios'
+import cors from 'cors'
 
+Vue.cors = cors
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
